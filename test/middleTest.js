f70a5f64bead28da431ea537a838a7b1cc33f86e
Alex Garin <lxg1992@gmail.com>
2019-09-23 18:41:59 +0000

    added an extra test to middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,7 +2,11 @@ const middle = require('../middle.js');
 const assert = require('chai').assert;
 
 describe("#middle", () => {
-  it('should get the middle of [1,2,3] to be 2', () => {
+  it('should get the middle of [1,2,3] to be [2]', () => {
     assert.deepEqual(middle([1,2,3]),[2])
+  })
+
+  it('should get the middle of [1,2,3,4] to be [2,3]', () => {
+    assert.deepEqual(middle([1,2,3,4], [2,3]))
   })
 })
