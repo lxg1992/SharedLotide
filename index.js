56c9c758432cc5d3652fee8b7a37d5656b93dd1f
Alex Garin <lxg1992@gmail.com>
2019-09-23 19:12:04 +0000

    Modified index to include all lotide exports

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
+const head                = require('./head');
+const tail                = require('./tail');
+const middle              = require('./middle');
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertEqual         = require('./assertEqual');
+const assertObjectsEqual  = require('./assertObjectsEqual');
+const countLetter         = require('./countLetter');
+const countOnly           = require('./countOnly');
+const eqArrays            = require('./eqArrays');
+const eqObjects           = require('./eqObjects');
+const findKey             = require('./findKey');
+const findKeyByValue      = require('./findKeyByValue');
+const flatten             = require('./flatten');
+const map                 = require('./map');
+const takeUntil           = require('./takeUntil');
+const without             = require('./without');
+
+
+
+
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetter       ,
+  countOnly         ,
+  eqArrays          ,
+  eqObjects         ,
+  findKey           ,
+  findKeyByValue    ,
+  flatten           ,
+  map               ,
+  takeUntil         ,
+  without           ,
+  assertEqual       
+  
 };
