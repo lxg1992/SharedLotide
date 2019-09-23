e37c797c977660f74c5dc04fc8cc72c18a852588
Alex Garin <lxg1992@gmail.com>
2019-09-23 17:35:35 +0000

    Finished middle refactoring

--- middle.js
+++ middle.js
@@ -1,36 +1,38 @@
-const assertArraysEqual = function(actual, expected) {
-  if (Array.isArray(actual) && Array.isArray(expected) && actual.length === expected.length) {
-    let isComparable = true;
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        isComparable = false;
-      }
-    }
-    if (isComparable) {
-      console.log(`Assert succeeded: ${actual} = ${expected}`);
+// const assertArraysEqual = function(actual, expected) {
+//   if (Array.isArray(actual) && Array.isArray(expected) && actual.length === expected.length) {
+//     let isComparable = true;
+//     for (let i = 0; i < actual.length; i++) {
+//       if (actual[i] !== expected[i]) {
+//         isComparable = false;
+//       }
+//     }
+//     if (isComparable) {
+//       console.log(`Assert succeeded: ${actual} = ${expected}`);
       
-    } else {
-      console.log(`Assert failed: ${actual} != ${expected}`);
+//     } else {
+//       console.log(`Assert failed: ${actual} != ${expected}`);
       
-    }
-  }
-};
+//     }
+//   }
+// };
 
-const eqArrays = function(actual, expected) {
-  if (Array.isArray(actual) && Array.isArray(expected) && actual.length === expected.length) {
-    let isComparable = true;
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        isComparable = false;
-      }
-    }
-    if (isComparable) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
+// const eqArrays = function(actual, expected) {
+//   if (Array.isArray(actual) && Array.isArray(expected) && actual.length === expected.length) {
+//     let isComparable = true;
+//     for (let i = 0; i < actual.length; i++) {
+//       if (actual[i] !== expected[i]) {
+//         isComparable = false;
+//       }
+//     }
+//     if (isComparable) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// };
+
+
 
 const middle = function(arrayInput) {
   let num = 0;
@@ -46,15 +48,8 @@ const middle = function(arrayInput) {
   }
 };
 
-console.log(middle([1]));
-console.log(middle([1,2]));
-console.log(middle([1,2,3]));
-console.log(middle([1,2,3,4]));
-console.log(middle([1,2,3,4,5]));
-console.log(middle([1,2,3,4,5,6]));
+module.exports = middle;
 
-assertArraysEqual(middle([1,2,3]),[2]);
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
+
 
 
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,10 +1,18 @@
 const assertArraysEqual = require('../assertArraysEqual.js');
+const middle = require('../middle.js');
 
-assertArraysEqual([1,2],[1,2]);
 
 let first = [1,2,3];
 let second = [1,2,3];
 let third = [1,2,'3'];
 
+console.log("assertArraysEqual() test")
+assertArraysEqual([1,2],[1,2]);
+
 assertArraysEqual(first, second);
 assertArraysEqual(second, third);
+
+console.log("Middle() test")
+assertArraysEqual(middle([1,2,3]),[2]);
+assertArraysEqual(middle([1,2,3,4]),[2,3]);
+assertArraysEqual(middle([1,2,3,4,5]),[3]);
